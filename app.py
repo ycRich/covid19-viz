355db2e2b487fdfbf132d00b32da45a5a6c2f1d8
Yichi Zhang <yczhang0523@gmail.com>
2020-03-25 03:59:01 -0400

    reformatted layout

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ from us_state_abbrev import us_state_abbrev
 from datetime import date, timedelta
 import utils
 from urllib.request import urlopen
-# import json
-# with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-#     counties = json.load(response)
+
 
 template='plotly_dark'
 colors = {
@@ -41,11 +39,6 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ]), 
     
     html.Br(),
-
-    # html.H4(children='Case Distribution Map', style={
-    #     'textAlign': 'center',
-    #     'color': colors['header']
-    # }),
 
     html.Div(className='row', style={'padding-left':'5%', 'padding-right':'5%'}, children=[
         html.Div(className='two columns', children=[
@@ -78,37 +71,25 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         ,
         html.Div(className='ten columns', style={'box-shadow': shadow}, children=[
             dcc.Graph(id='map'),
-            dcc.Graph(id='barchart')
+            dcc.Graph(id='barchart'),
+            dcc.Graph(
+                id='trend-confirmed',
+                figure=px.area(timeseries['confirmed'], x='date', y='Number of Cases',
+                                color='Province/State', template=template, title='Trend of Confirmed Cases (as of 03/22/2020)')
+            ),
+            dcc.Graph(
+                id='trend-deaths',
+                figure=px.area(timeseries['deaths'], x='date', y='Number of Cases',
+                                color='Province/State', template=template, title='Trend of Deaths (as of 03/22/2020)')
+            ),
+            html.Br(),
+            html.P("""This tracker takes data from the repository of the 2019 Novel Coronavirus Visual Dashboard operated
+                by the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE). It will apply 
+                necessary cleansing/reformatting to make it use in traditional relational databases and data visualization tools.""", 
+                style={'color':'#707070', 'padding-left':'5%', 'padding-right': '5%'})
         ])
     ]),
 
-    html.Br(),
-    # html.H4(children='Trend By States', style={
-    #     'textAlign': 'center',
-    #     'color': colors['header']
-    # }),
-    html.Div(className='row', style={'padding-left': '5%', 'padding-right':'5%'}, children=[
-        dcc.Graph(
-            id='trend-confirmed',
-            className='six columns',
-            figure=px.area(timeseries['confirmed'], x='date', y='Number of Cases',
-                            color='Province/State', template=template, title='Trend of Confirmed Cases (as of 03/22/2020)'),
-            style={'box-shadow': shadow}
-        ),
-        dcc.Graph(
-            id='trend-deaths',
-            className='six columns',
-            figure=px.area(timeseries['deaths'], x='date', y='Number of Cases',
-                            color='Province/State', template=template, title='Trend of Deaths (as of 03/22/2020)'),
-            style={'box-shadow': shadow}
-        )
-    ]),
-
-    html.Br(),
-    html.P("""This tracker takes data from the repository of the 2019 Novel Coronavirus Visual Dashboard operated
-        by the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE). It will apply 
-        necessary cleansing/reformatting to make it use in traditional relational databases and data visualization tools.""", 
-        style={'color':colors['text'], 'padding-left':'5%', 'padding-right':'5%'})
 ])
 
 @app.callback(
@@ -119,51 +100,38 @@ def update_map(case_type, selected_date):
     y, m, d = selected_date.split('-')
     report = utils.load_state_daily_report(m+'-'+d+'-'+y)
     if pd.to_datetime(selected_date) <= pd.to_datetime('03-21-2020'):
-        fig = px.choropleth(
-            report, title='Case Distribution Map',
-            locationmode='USA-states',
-            color = np.log10(report[case_type]),
-            locations=report['Province/State'],
-            hover_data=['Confirmed', 'Deaths', 'Recovered', 'Active'], 
-            hover_name='Province/State',
-            color_continuous_scale='YlOrRd',
-            template=template
-        )
-        fig.update_layout(
-                showlegend = True,
-                geo_scope='usa',
-                coloraxis_colorbar=dict(
-                    title='# of Cases',
-                    tickvals=[1, 2, 3, 4, 5],
-                    ticktext=['10', '100', '1k', '10k','100k'],
-                    len=0.75, thickness=10
-                ),
-                margin={"r":0,"t":75,"l":0,"b":0}
-        )
+        location = 'Province/State'
+        tickvals = [1, 2, 3, 4, 5]
+        ticktext=['10', '100', '1k', '10k','100k']
+        lat, lon = 'Latitude', 'Longitude'
     else:
-        report = report.sort_values(case_type)
-        fig = px.scatter_geo(
-            report, title='Case Distribution Map',
-            lat=report['Lat'], lon=report['Long_'], scope='usa',
-            color = np.log10(report[case_type]+1),opacity=0.75,
-            size = report[case_type]**0.5,
-            hover_data=['Confirmed', 'Deaths', 'Recovered', 'Active'], 
-            hover_name='Combined_Key',
-            color_continuous_scale='YlOrRd',
-            template=template,
-        )
-        fig.update_layout(
-            showlegend = True,
-            geo_scope='usa',
-            coloraxis_colorbar=dict(
-                title='# of Cases',
-                tickvals=[0, 1, 2, 3, 4],
-                ticktext=['0', '10', '100', '1k','10k'],
-                len=0.75, thickness=10
+        # report = report.sort_values(case_type)
+        location = 'Combined_Key'
+        tickvals = [1, 2, 3, 4]
+        ticktext=['10', '100', '1k', '10k']
+        lat, lon = 'Lat', 'Long_'
+    fig = px.scatter_geo(
+        report, title='Case Distribution Map',
+        lat=report[lat], lon=report[lon], scope='usa',
+        color = np.log10(report[case_type]+1),opacity=0.75,
+        size = report[case_type]**0.5,
+        hover_data=['Confirmed', 'Deaths', 'Recovered', 'Active'], 
+        hover_name=location,
+        color_continuous_scale='YlOrRd',
+        template=template,
+    )
+    fig.update_layout(
+        showlegend = True,
+        geo_scope='usa',
+        coloraxis_colorbar=dict(
+            title='# of Cases',
+            tickvals=[0, 1, 2, 3, 4],
+            ticktext=['0', '10', '100', '1k','10k'],
+            len=0.75, thickness=10
 
-            ),
-            margin={"r":0,"t":75,"l":0,"b":0}
-        )
+        ),
+        margin={"r":0,"t":75,"l":0,"b":0}
+    )
     return fig
 
 
